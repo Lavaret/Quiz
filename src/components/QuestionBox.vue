de47fb860eaf2771788a219165dc89e7caddfe0a
Lavaret <czarnynozyk@gmail.com>
2019-10-26 10:30:13 +0200

    Switching questions

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,17 +1,25 @@
 <template>
   <section class="question">
     <div class="question-wrap">
-      <h2>Some question here?</h2>
+      <span class="category">{{currentQuestion.category}}</span>
+      <h2>{{currentQuestion.question}}</h2>
       <div class="line"/>
       <div class="question-list">
         List of questions
       </div>
       <button>Save</button>
-      <button>Next</button>
+      <button @click="onNext">Next</button>
     </div>
   </section>
 </template>
-
+<script>
+  export default {
+    props: {
+      currentQuestion: Object,
+      onNext: Function
+    }
+  }
+</script>
 <style type="scss" scoped>
   h2 {
     font-weight: normal;
@@ -22,7 +30,13 @@
       margin: 5px;
       height: 40px;
       width: 80px;
-      background-color: 
+      background-color:
+  }
+
+  .category {
+    color: #6f7f94;
+    font-size: 16px;
+    font-style: italic;
   }
 
   .question-wrap {
@@ -32,6 +46,7 @@
   }
 
   .question {
+    padding: 15px;
     display: flex;
     justify-content: center;
   }
