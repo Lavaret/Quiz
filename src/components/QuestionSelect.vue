fd3da4f831650ef229b4e6b2d049efab568fa5e9
Lavaret <czarnynozyk@gmail.com>
2019-10-27 15:56:24 +0100

    finish page

---
+++ src/components/QuestionSelect.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="finish">
+    <h2>Finish!</h2>
+    <button @click="startGame">Start again</button>
+  </section>
+</template>
+<script>
+  export default {
+    props: {
+      getQuestions: Function
+    },
+    methods: {
+      startGame() {
+        location.reload();
+      }
+    }
+  }
+</script>
+
+<style type="css" scoped>
+  .finish {
+    text-align: center;
+    margin-top: 50px;
+  }
+
+  button {
+    width: 150px;
+    height: 50px;
+  }
+</style>
